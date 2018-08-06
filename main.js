93ef224d129f6dbee5af48b7e154f01a595f27de
Michał Kowalik <michal.kowalik.94@gmail.com>
2018-08-06 23:40:38 +0200

    Change metode add form

--- main.js
+++ main.js
@@ -2,25 +2,32 @@
 var form = {};
 document.addEventListener("DOMContentLoaded", evt => {
 	readyFuntion();
+	AddProto();
 });
+
+function AddProto(){
+	Array.prototype.clean = function(deleteValue) {
+	for (var i = 0; i < this.length; i++) {
+		if (this[i] == deleteValue){
+			this.splice(i, 1);
+			i--;
+		}
+	}
+	return this;
+	};
+}
 
 function readyFuntion(){
 	openConectionDB();
-	document.querySelector('#addMain').addEventListener('click', addMain);
+	document.querySelector('#addMain').addEventListener('click', () => document.querySelector('.container').insertAdjacentHTML('beforeend',addForm()));
 	document.addEventListener('click',e => {
-	 	if (e.target.tagName.toLocaleLowerCase() == 'input') e.target.classList.add('visited');
-	 	if(e.target.classList.contains('addSubInput')) addSubInput(e);
-	 	if(e.target.classList.contains('delete')) Delete(e);
+		if (e.target.tagName.toLocaleLowerCase() == 'input') e.target.classList.add('visited');
+		if(e.target.classList.contains('addSubInput')) addSubInput(e);
+		if(e.target.classList.contains('delete')) Delete(e);
 	 });
 	document.body.addEventListener('DOMSubtreeModified',() => saveForm());
 }
-var event;
-function addMain(e){
-	const PossibleTypes = ['Text', 'Number', 'Yes/No'];
-	let options = PossibleTypes.map((value) => (`<option value="${value}">${value}</option>`)).toString();
-	document.querySelector('.container').insertAdjacentHTML('beforeend', addTypes(options, null));
-}
-//dataset.index
+
 function validate(e){
 	let complete;
 	let form = e.target.closest('div[class="form"]');
@@ -66,14 +73,15 @@ function testSave(){
 	}
 }
 
-
+	var savedHtml;
+	var items = {};
 function testRead(){
 	save = JSON.parse(localStorage.getItem('test'));
 
 	var tree = iterate(save.Question0);
-	var savedHtml;
 	function iterate(element){
 		console.log(element.Question , element.Type, element.CodnitionType, element.CodnitionValue);
+		console.log(addFormTest(element.CodnitionType));
 		if (element.hasOwnProperty('children')){
 			for (var j=0; j < element.children.length; j++){
 				iterate(element.children[j]);
@@ -84,80 +92,22 @@ function testRead(){
 
 }
 
-
-Array.prototype.clean = function(deleteValue) {
-  for (var i = 0; i < this.length; i++) {
-    if (this[i] == deleteValue) {         
-      this.splice(i, 1);
-      i--;
-    }
-  }
-  return this;
-};
-
-
 function showModal(){
 	var modal = document.querySelector('#ModalError');
 	var span = document.querySelector(".close");
 		span.onclick = () => {modal.style.display = "none";}
-    modal.style.display = "block";
-    window.onclick = (e) => {if(e.target == modal) modal.style.display = "none";} 
+	modal.style.display = "block";
+	window.onclick = (e) => {if(e.target == modal) modal.style.display = "none";} 
 }
 
 function addSubInput(e){
 	if (!validate(e)) return showModal();
-	const PossibleTypes = ['Text', 'Number', 'Yes/No'];
-	let options = PossibleTypes.map((value) => (`<option value="${value}">${value}</option>`)).toString();
-	let type = e.target.closest('div[class="form"]').querySelector('select[name="Type"]').selectedOptions[0].value;
-	event = e;
-	e.target.closest('div[class="Question"]').insertAdjacentHTML('beforeend', addTypes(options, type));
+	const ParentType = e.target.closest('div[class="Question"]').querySelector('[name="Type"]').value
+	e.target.closest('div[class="Question"]').insertAdjacentHTML('beforeend', addForm(ParentType));
 }
 
 function Delete(e){
 	e.target.closest('div[class="Question"]').remove();
-}
-
-function condition(type){
-	if (!type) return '';
-	let options = {'Text': ['Equals'], 'Number': ['Equals', 'Greater than', 'Less than'], 'Yes/No': ['Equals']};
-	let optionsType = options[type].map(value => (`<option value="${value}">${value}</option>`)).toString();
-	let inputOrSelect = (type == 'Yes/No') ? `<select class="right selectBool" name="CodnitionValue"><option selected disabled>Please select type</option><option value="Yes">Yes</option><option value="No">No</opton></select>`:`<input type="${type.toLowerCase()}" class="right" required name="CodnitionValue">`;
-	let additionalRow = `
-		<div class="row">
-			<label class="">Codnition</label>
-			<select class="" name="Codnition">
-				<option selected disabled>Please select type</option>
-				${optionsType}
-			</select>
-			${inputOrSelect}
-		</div>
-	`
-	return additionalRow;
-}
-
-function addTypes(option, type){
-	let newForm = `
-		<div class="Question">
-			<div class="form">
-				${condition(type)}
-				<div class="row">
-					<label class="">Question</label>
-					<input type="text" class="right" name="Question" required>
-				</div>
-				<div class="row">
-					<label class="">Type</label>
-					<select class="right" name="Type">
-					<option selected disabled>Please select type</option>
-					${option}
-					</select>
-				</div>
-				<div class="rowButton">
-					<button type="button" class="button addSubInput">Add Sub-Input</button>
-					<button type="button" class="button delete">Delete</button>
-				</div>
-			</div>
-		</div>`;
-	return newForm;
 }
 
 function saveForm(){
@@ -168,4 +118,47 @@ function saveForm(){
 		// save.querySelectorAll('.visited').forEach(element => element.classList.remove('visited'));
 		// save.querySelectorAll('.wrong').forEach(element => element.classList.remove('wrong'));
 	saveToindexedDB(save);
+}
+
+function addForm(parentType = null){
+	console.log(parentType)
+	const PossibleTypes = ['Text', 'Number', 'Yes/No'];
+	let rowCodntionAndValue = (element) => {
+		if (!element) return '';
+		let ConditionValue = (element == 'Yes/No') ? `<select class="right selectBool" name="CodnitionValue"><option selected disabled>Please select type</option><option value="Yes">Yes</option><option value="No">No</opton></select>`:`<input type="${element.toLowerCase()}" class="right" required name="CodnitionValue">`;
+		let options = {'Text': ['Equals'], 'Number': ['Equals', 'Greater than', 'Less than'], 'Yes/No': ['Equals']};
+		let ConditionSelect = options[element].map(value => (`<option value="${value}">${value}</option>`)).join('');
+		return (`
+		<div class="row">
+			<label class="">Codnition</label>
+			<select class="" name="Codnition">
+				<option selected disabled>Please select type</option>
+				${ConditionSelect}
+			</select>
+			${ConditionValue}
+		</div>
+		`);
+	}
+	return (`
+		<div class="Question">
+			<div class="form">
+			${rowCodntionAndValue(parentType)}
+			<div class="row">
+				<label class="">Question</label>
+				<input type="text" class="right" name="Question" required>
+			</div>
+			<div class="row">
+				<label class="">Type</label>
+				<select class="right" name="Type">
+				<option selected disabled>Please select type</option>
+				${PossibleTypes.map((value) => (`<option value="${value}">${value}</option>`)).join('')}
+				</select>
+			</div>
+			<div class="rowButton">
+				<button type="button" class="button addSubInput">Add Sub-Input</button>
+				<button type="button" class="button delete">Delete</button>
+			</div>
+			</div>
+		</div>
+	`);
 }
